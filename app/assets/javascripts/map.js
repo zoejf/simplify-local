fd065dd4b09b4c37e4b4c9fe67b57f5d8184a0ec
jhars <jjharlan06@gmail.com>
2015-09-29 13:22:23 -0700

    color coded categories

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,4 @@
 
-    // $.get("/results.json",function(data){
-    // 		var lat = 37.7577;
-    // 		var lng = -122.4376;
-    // 		// biz();
-    	    // showMarker(lng, lat);
-
    // Enables string interpolation on string objects ect..
     String.prototype.supplant = function (o) {
       return this.replace(/{([^{}]*)}/g,
@@ -29,46 +23,4 @@
 
     var geocoder = L.mapbox.geocoder('mapbox.places-v1');
 
-    var showMarker = function(lat, lng, link, name, previewImage, phone, productTitle, productPrice) {
-      L.mapbox.featureLayer({
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [lng, lat]
-        },
-        properties: {
-        description:  "<div id='biz'><a href='" + link + "' id='bizName'>" + name + "</a>" + "<br><strong> Featured Product Price: " + productPrice + "</strong>" + "<br>" + "<img src='" + previewImage + "' id='bizImage'>" + "<br>" + productTitle +"</div>",
-        // description: "<a href='" + link + "'>" + name + "</a>" + "<br>" + phone,
-          'marker-size': 'medium',
-          'marker-color': '#0967A9',
-          'marker-symbol': 'circle-stroked'
-        }
-      }).addTo(map);
-    };
-
-      $.ajax({
-            url: 'https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&filters=category:food-and-drink&offset=0&limit=50',
-            dataType: 'jsonp',
-            success: function(biz2Data) {
-            var data = biz2Data.deals;
-             for (i = 0; i < data.length; i++) {
-                // console.log(data["options"][i]["redemptionLocations"][i]["lat"]);
-                
-                
-                var bLat = data[i]['options'][0]["redemptionLocations"][0]["lat"];
-                var bLng = data[i]['options'][0]["redemptionLocations"][0]["lng"];
-                
-                var name = data[i]["merchant"]["name"];
-                var link = data[i]["dealUrl"];
-                var phone = data[i]["options"][0]["redemptionLocations"][0]["phoneNumber"];
-                var previewImage = data[i]["mediumImageUrl"];
-                var productTitle = data[i]["title"];
-                var productPrice = data[i]["options"][0]["price"]["formattedAmount"];
-                console.log(productTitle);
-                console.log(productPrice);
-
-                showMarker(bLat,bLng, link, name, previewImage, phone, productTitle, productPrice);
-             };
-            }
-          });
-             
+  
--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,41 +1,41 @@
-// $(function() {
-//   Digits.init({ consumerKey: 'F6W9oHNeu1oPLPwtmAZWErWkY' });
-//   console.log('doc loaded')
-//   $('#loginButton').click(function(event) {
-//     event.preventDefault();
-//     console.log('login button')
-//     Digits.logIn()
-//       .done(function(response) {
-//         // Send headers to your server and validate user by calling Digits’ API
-//         var oAuthHeaders = response.oauth_echo_headers;
-//         var verifyData = {
-//           authHeader: oAuthHeaders['X-Verify-Credentials-Authorization'],
-//           apiUrl: oAuthHeaders['X-Auth-Service-Provider'],
-//           //name, email, enough to create user
-//         };
+$(function() {
+  Digits.init({ consumerKey: 'F6W9oHNeu1oPLPwtmAZWErWkY' });
+  console.log('doc loaded')
+  $('#loginButton').click(function(event) {
+    event.preventDefault();
+    console.log('login button')
+    Digits.logIn()
+      .done(function(response) {
+        // Send headers to your server and validate user by calling Digits’ API
+        var oAuthHeaders = response.oauth_echo_headers;
+        var verifyData = {
+          authHeader: oAuthHeaders['X-Verify-Credentials-Authorization'],
+          apiUrl: oAuthHeaders['X-Auth-Service-Provider'],
+          //name, email, enough to create user
+        };
 
-//         $.ajax({
-//             url: '/sessions',
-//             type: 'POST',
-//             data: verifyData,
-//             success: function(data) {
-//               if(data.email === null) {
-//                 // this is a new user
-//                 window.location = '/';
-//               } else {
-//                 // we know about this user
-//                 window.location = '/';
-//               }
-//               console.log('success');
-//               console.log('session.js');
-//             },
-//             error: function(xhr, textStatus, errorThrown) {
-//               console.log('error', xhr, textStatus);
-//             }
-//         });
-//       })
-//     .fail(function(error) {
-//       console.log('an error occured:', error);
-//     });
-//   });
-// });
+        $.ajax({
+            url: '/sessions',
+            type: 'POST',
+            data: verifyData,
+            success: function(data) {
+              if(data.email === null) {
+                // this is a new user
+                window.location = '/';
+              } else {
+                // we know about this user
+                window.location = '/';
+              }
+              console.log('success');
+              console.log('session.js');
+            },
+            error: function(xhr, textStatus, errorThrown) {
+              console.log('error', xhr, textStatus);
+            }
+        });
+      })
+    .fail(function(error) {
+      console.log('an error occured:', error);
+    });
+  });
+});
